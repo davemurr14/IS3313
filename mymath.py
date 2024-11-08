845e36983dcecffa5318ea9e88904b8a73194435
davemurr14 <122504223@umail.ucc.ie>
2024-11-08 12:22:26 +0000

    INITIAL COMMIT

--- mymath.py
+++ mymath.py
@@ -1,14 +1,19 @@
 def add_three(a, b, c):
-    return a+b+c
+    return -1
 
 def subtract_three(a, b, c):
-    return a-b-c
+    return -1
 
 def multiply_three(a, b, c):
-    return a*b*c
+    return -1
 
 def divide_three(a, b, c):
-    return a/b/c
+    return -1
 
-def power(a, b):
-    return a**b
+def square(a):
+    #return a**2
+    return -1
+
+def cube(a):
+    #return a**3
+    return -1
